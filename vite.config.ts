743a3639860fafde0f67e1ed8031d78c4411952b
Kenya Masuko <ken12rug@gmail.com>
2024-07-21 21:29:12 +0900

    chore: support ssg

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import pages from '@hono/vite-cloudflare-pages'
-import adapter from '@hono/vite-dev-server/cloudflare'
-import honox from 'honox/vite'
-import { defineConfig } from 'vite'
+import ssg from "@hono/vite-ssg";
+import honox from "honox/vite";
+import { defineConfig } from "vite";
+
+const entry = "./app/server.ts";
 
 export default defineConfig({
-  plugins: [honox({ devServer: { adapter } }), pages()]
-})
+	plugins: [honox(), ssg({ entry })],
+});
